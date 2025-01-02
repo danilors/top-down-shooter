4b69a80993cd41d18d15bbec0828f97360bedb15
Danilo Silva <danilo170@hotmail.com>
2025-01-02 00:17:27 -0300

    add bullet enemy colision

--- src/main.lua
+++ src/main.lua
@@ -13,11 +13,16 @@ function love.load()
 
     zombies = {}
     zombieSpeed = 140
+
+    bullets = {}
+    bulletSpeed = 500
 end
 
 function love.update(dt)
     playerKeyBoardActions(dt)
     zombieMovement(dt)
+    bulletsMovement(dt)
+    zombieBulletsColision(dt)
 end
 
 function love.draw()
@@ -43,6 +48,19 @@ function love.draw()
             nil,
             sprites.zombie:getWidth() / 2,
             sprites.zombie:getHeight() / 2
+        )
+    end
+
+    for i, b in ipairs(bullets) do
+        love.graphics.draw(
+            sprites.bullet,
+            b.x,
+            b.y,
+            nil,
+            0.5,
+            nil,
+            sprites.bullet:getWidth() / 2,
+            sprites.bullet:getHeight() / 2
         )
     end
 end
@@ -82,6 +100,7 @@ function spawnZombie()
     zombie.x = math.random(0, love.graphics.getWidth())
     zombie.y = math.random(0, love.graphics.getHeight())
     zombie.speed = zombieSpeed
+    zombie.dead = false
     table.insert(zombies, zombie)
 end
 
@@ -89,8 +108,8 @@ function zombieMovement(dt)
     for i, z in ipairs(zombies) do
         z.x = z.x + (math.cos(zombiePlayerAngle(z)) * z.speed * dt)
         z.y = z.y + (math.sin(zombiePlayerAngle(z)) * z.speed * dt)
-   
-        if distanceBetween(z.x, z.y, player.x, player.y) < 100 then
+
+        if distanceBetween(z.x, z.y, player.x, player.y) < 50 then
             for j, z in ipairs(zombies) do
                 zombies[j] = nil
             end
@@ -98,6 +117,60 @@ function zombieMovement(dt)
     end
 end
 
+function bulletsMovement(dt)
+    for i, b in ipairs(bullets) do
+        b.x = b.x + (math.cos(b.direction) * b.speed * dt)
+        b.y = b.y + (math.sin(b.direction) * b.speed * dt)
+    end
+
+    for i = #bullets, 1, -1 do
+        local b = bullets[i]
+        if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+            table.remove(bullets, i)
+        end
+    end
+end
+
 function distanceBetween(x1, y1, x2, y2)
     return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+end
+
+function spawnBullet()
+    local bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = bulletSpeed
+    bullet.dead = false
+    bullet.direction = playerMouseAngle()
+    table.insert(bullets, bullet)
+end
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        spawnBullet()
+    end
+end
+
+function zombieBulletsColision(dt)
+    for i, z in ipairs(zombies) do
+        for j, b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true;
+                b.dead = true
+            end
+        end
+    end
+    
+    for i = #zombies, 1, -1 do
+        local z = zombies[i]
+        if z.dead == true then
+            table.remove(zombies, i)
+        end
+    end
+    for i = #bullets, 1, -1 do
+        local b = bullets[i]
+        if b.dead == true then
+            table.remove(bullets, i)
+        end
+    end
 end
