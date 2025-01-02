350ab80bdb2a5e4ca580481907728a50373a6b62
Danilo Silva <danilo170@hotmail.com>
2025-01-01 21:39:24 -0300

    add player movement

--- src/main.lua
+++ src/main.lua
@@ -4,13 +4,31 @@ function love.load()
     sprites.bullet = love.graphics.newImage("assets/sprites/bullet.png")
     sprites.player = love.graphics.newImage("assets/sprites/player.png")
     sprites.zombie = love.graphics.newImage("assets/sprites/zombie.png")
+
+    player = {}
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+    player.speed = 5;
     
 end
 
 function love.update()
-    
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+    end
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed
+    end
 end
 
 function love.draw()
-    
+    love.graphics.draw(sprites.background, 0, 0)
+
+    love.graphics.draw(sprites.player, player.x, player.y)
 end
