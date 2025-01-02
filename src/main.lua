76af6a077f53b625400b76e001a57bb1251f074b
Danilo Silva <danilo170@hotmail.com>
2025-01-01 21:13:15 -0300

    add and config assets

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,9 @@
 function love.load()
+    sprites = {}
+    sprites.background = love.graphics.newImage("assets/sprites/background.png")
+    sprites.bullet = love.graphics.newImage("assets/sprites/bullet.png")
+    sprites.player = love.graphics.newImage("assets/sprites/player.png")
+    sprites.zombie = love.graphics.newImage("assets/sprites/zombie.png")
     
 end
 
